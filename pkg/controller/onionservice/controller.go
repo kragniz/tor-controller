f9a85664917cb6dc938d1ccf8a68569b26a7ce8b
Louis Taylor <louis@kragniz.eu>
2018-05-27 16:25:44 +0100

    Watch deployments

--- pkg/controller/onionservice/controller.go
+++ pkg/controller/onionservice/controller.go
@@ -5,7 +5,9 @@ import (
 
 	"github.com/kubernetes-sigs/kubebuilder/pkg/controller"
 	"github.com/kubernetes-sigs/kubebuilder/pkg/controller/types"
+	corelisters "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/record"
+	appsv1 "k8s.io/api/apps/v1"
 
 	torv1alpha1 "github.com/kragniz/tor-controller/pkg/apis/tor/v1alpha1"
 	torv1alpha1client "github.com/kragniz/tor-controller/pkg/client/clientset/versioned/typed/tor/v1alpha1"
@@ -28,15 +30,13 @@ func (bc *OnionServiceController) Reconcile(k types.ReconcileKey) error {
 type OnionServiceController struct {
 	onionserviceLister torv1alpha1lister.OnionServiceLister
 	onionserviceclient torv1alpha1client.TorV1alpha1Interface
+
 	// recorder is an event recorder for recording Event resources to the
 	// Kubernetes API.
 	onionservicerecorder record.EventRecorder
 }
 
-// ProvideController provides a controller that will be run at startup.  Kubebuilder will use codegeneration
-// to automatically register this controller in the inject package
 func ProvideController(arguments args.InjectArgs) (*controller.GenericController, error) {
-	// INSERT INITIALIZATIONS FOR ADDITIONAL FIELDS HERE
 	bc := &OnionServiceController{
 		onionserviceLister: arguments.ControllerManager.GetInformerProvider(&torv1alpha1.OnionService{}).(torv1alpha1informer.OnionServiceInformer).Lister(),
 
@@ -54,17 +54,10 @@ func ProvideController(arguments args.InjectArgs) (*controller.GenericController
 		return gc, err
 	}
 
-	// IMPORTANT:
-	// To watch additional resource types - such as those created by your controller - add gc.Watch* function calls here
-	// Watch function calls will transform each object event into a OnionService Key to be reconciled by the controller.
-	//
-	// **********
-	// For any new Watched types, you MUST add the appropriate // +kubebuilder:informer and // +kubebuilder:rbac
-	// annotations to the OnionServiceController and run "kubebuilder generate.
-	// This will generate the code to start the informers and create the RBAC rules needed for running in a cluster.
-	// See:
-	// https://godoc.org/github.com/kubernetes-sigs/kubebuilder/pkg/gen/controller#example-package
-	// **********
+	if err := gc.WatchControllerOf(&appsv1.Deployment{}, eventhandlers.Path{bc.LookupFoo},
+        predicates.ResourceVersionChanged); err != nil {
+        return gc, err
+}
 
 	return gc, nil
 }
