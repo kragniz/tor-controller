bce5cacda332cac3c25e5d7a10c86ab8f9fdb028
Louis Taylor <louis@kragniz.eu>
2018-06-04 23:31:21 +0100

    Manually add rbac informers

--- pkg/inject/inject.go
+++ pkg/inject/inject.go
@@ -1,6 +1,8 @@
 package inject
 
 import (
+	rbacv1 "k8s.io/api/rbac/v1"
+
 	injectargs "github.com/kubernetes-sigs/kubebuilder/pkg/inject/args"
 	"github.com/kubernetes-sigs/kubebuilder/pkg/inject/run"
 
@@ -23,6 +25,15 @@ func RunAll(rargs run.RunArguments, iargs args.InjectArgs) error {
 			return err
 		}
 	}
+
+	if err := iargs.ControllerManager.AddInformerProvider(&rbacv1.Role{}, iargs.KubernetesInformers.Rbac().V1().Roles()); err != nil {
+		return err
+	}
+
+	if err := iargs.ControllerManager.AddInformerProvider(&rbacv1.RoleBinding{}, iargs.KubernetesInformers.Rbac().V1().RoleBindings()); err != nil {
+		return err
+	}
+
 	Injector.Run(rargs)
 	<-rargs.Stop
 	return nil
